f801bf252070ccb3b68170872dbf6c4929b7dacf
sunfkny <30853461+sunfkny@users.noreply.github.com>
2022-06-08 09:08:01 +0800

    format

--- BackgroundMuter.py
+++ BackgroundMuter.py
@@ -1,21 +1,21 @@
-import win32gui
 import time
+
 import psutil
-from pycaw.pycaw import AudioUtilities
+import win32gui
 import win32process
+from pycaw.pycaw import AudioUtilities
+
 
 def get_config():
-    process_name_list = ['YuanShen.exe', 'GenshinImpact.exe']
-    file_name = 'BackgroundMuter.txt'
-    encoding = 'utf-8'
+    process_name_list = ["YuanShen.exe", "GenshinImpact.exe"]
+    file_name = "BackgroundMuter.txt"
+    encoding = "utf-8"
     try:
         process_name_list = open(file_name, "r", encoding=encoding).read().strip().split("\n")
     except:
         open(file_name, "w", encoding=encoding).write("\n".join(process_name_list))
     return process_name_list
 
-def get_pid():
-    pass
 
 background_mute_process = get_config()
 print("Background mute process:", background_mute_process)
@@ -29,14 +29,14 @@ while 1:
     sessions = AudioUtilities.GetAllSessions()
     for session in sessions:
         pname = session.Process.name() if session.Process else None
-        if not pname: # @%SystemRoot%\\System32\\AudioSrv.Dll,-202
+        if not pname:  # @%SystemRoot%\\System32\\AudioSrv.Dll,-202
             continue
 
         volume = session.SimpleAudioVolume
         mute = foreground_name != pname
         is_mute = volume.GetMute()
         is_changed = is_mute != mute
-        
+
         if pname in background_mute_process:
             if is_changed:
                 volume.SetMute(mute, None)
