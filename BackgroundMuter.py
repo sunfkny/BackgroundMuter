417d21b4b34da9a7c37d40cdcc67b737dddbeda8
sunfkny <30853461+sunfkny@users.noreply.github.com>
2022-06-12 16:35:24 +0800

    bat launch

--- BackgroundMuter.py
+++ BackgroundMuter.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 import psutil
 import win32gui
@@ -18,14 +19,8 @@ def get_config():
 
 
 background_mute_process = get_config()
-print("Background mute process:", background_mute_process)
-while 1:
-    pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
-    if pid[0] == 0:
-        print("No foreground window")
-        time.sleep(0.5)
-        continue
-    foreground_name = psutil.Process(pid[-1]).name()
+
+def unset_mute():
     sessions = AudioUtilities.GetAllSessions()
     for session in sessions:
         pname = session.Process.name() if session.Process else None
@@ -33,12 +28,44 @@ while 1:
             continue
 
         volume = session.SimpleAudioVolume
-        mute = foreground_name != pname
-        is_mute = volume.GetMute()
-        is_changed = is_mute != mute
-
         if pname in background_mute_process:
-            if is_changed:
-                volume.SetMute(mute, None)
-                print(f"{pname} SetMute {mute}")
+            volume.SetMute(False, None)
+            print(f"{pname} SetMute False")
+            return True
+    return False
+
+def main():
+    print("Background mute process:", background_mute_process)
+    while True:
+        pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())  # type: ignore
+        if pid[0] == 0:
+            print("No foreground window")
             time.sleep(0.5)
+            continue
+        foreground_name = psutil.Process(pid[-1]).name()
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            pname = session.Process.name() if session.Process else None
+            if not pname:  # @%SystemRoot%\\System32\\AudioSrv.Dll,-202
+                continue
+
+            volume = session.SimpleAudioVolume
+            mute = foreground_name != pname
+            is_mute = volume.GetMute()
+            is_changed = is_mute != mute
+
+            if pname in background_mute_process:
+                if is_changed:
+                    volume.SetMute(mute, None)
+                    print(f"{pname} SetMute {mute}")
+                time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv)>1:
+            unset_mute()
+        else:
+            main()
+    except KeyboardInterrupt:
+        print("Bye")
